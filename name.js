eab82ca337f7ea562eaa7b873cd24a8b5e5e49a1
tohidul <roman20222324@gmail.comProfile imageroman20222324@gmail.com>
2024-05-31 19:22:12 +0600

    first commit

---
+++ name.js
@@ -0,0 +1,114 @@
+//! Quz-1
+//? Create a variable called carName, assign the value Volvo to it.
+
+let carName = "Volvo" ; 
+
+console.log(carName)
+
+//! Quz-2
+//? On one single line, declare three variables with the following names and values:
+
+
+/*
+let firstName = "John";  // write variable name
+let lastName = "Doe" //write variable values 
+let age = 35 ; // write variable name and values both 
+
+
+let add = firstName + ' '+ lastName +' '+ age; // akane tintike add korahoulo
+console.log(add);
+*/
+//! Quz-3
+
+//? Use the correct assignment operator that will result in x being 50 (same as x = x * y). 
+
+// x = 10;
+// y = 5;
+//  x*=y; // what will be the value of x ? 
+// console.log(x);
+
+//! Quz-4
+
+//? Use comments to describe the correct data type of the following variables:
+
+// let length = 16; // data type ? //?Ans  typeof number
+// console.log(typeof length);
+// let lastName = "Johnson"; // data type ? //?Ans typeof string
+// console.log(typeof lastName);
+// const x = {
+//   firstName: "John",  
+//   lastName: "Doe"
+// };    // data type ? //?Ans typeof object
+// console.log(typeof x);
+
+//! Quz-5
+//? Execute the function named myFunction.
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+console.log(myFunction);
+// call the function and see the output 
+
+//! Quz-6
+//? 1. Create an object called person with name = John, age = 50, Then,   access the object to 
+
+
+const person = {
+    nome: 'John',
+    aeg: 50,
+}
+let mot = person.nome + " is "+ person.aeg;
+console.log(mot)
+
+//! Quz-7
+//?The <button> element should do something when someone clicks on it. Try to fix it!
+function myFunction(){
+    alert('Assalamu-alaicom')
+}
+
+//! Quz-8
+//? 1. Alert the number of items in an array, using the correct Array property: 
+
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+// cars.join('#')
+// console.log(cars)
+
+ //? 2. Change the first item of Brand to "Ford".
+
+// 	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+//     Brand[0]="Ford"
+//     console.log(Brand)
+
+
+//! Quz-9
+
+
+// 1. Use the correct Math method to create a random number.
+
+// 2. Use the correct Math method to return the largest number of 10 and 20.
+// let met = [ [2,3,4,5,,6,3,5,4,56,7,8,9] , [4,4,5,3,8,8,9]]
+// met.flat
+// console.log(met)
+//? 3. Use the correct Math method to get the square root of 9.
+let a = 9;
+let y = 3;
+let x = a*y;
+let d =x/a;
+console.log(d)
+
+
+//! Quz-10
+
+// 1. Choose the correct comparison operator to alert true, when x is greater than y. 
+//! x = 10;
+// y = 5;
+
+// !alert() // ? 
+
+
+
+// 2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+
+
+
